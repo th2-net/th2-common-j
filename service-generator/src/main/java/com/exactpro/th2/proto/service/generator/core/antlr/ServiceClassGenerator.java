03be2be393564331d90c1004a0e261fad8d5ec52
ilya loginov <ilya.loginov@exactprosystems.com>
2020-07-06 19:34:26 +0400

    [JIRA] TH2-297. Fix. Generate services methods name starting from lower case character.

--- service-generator/src/main/java/com/exactpro/th2/proto/service/generator/core/antlr/ServiceClassGenerator.java
+++ service-generator/src/main/java/com/exactpro/th2/proto/service/generator/core/antlr/ServiceClassGenerator.java
@@ -1,24 +1,20 @@
-/*
+/*****************************************************************************
  * Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
- * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *****************************************************************************/
+
 package com.exactpro.th2.proto.service.generator.core.antlr;
 
-import com.exactpro.th2.proto.service.generator.core.antlr.descriptor.ServiceDescriptor;
-import com.exactpro.th2.proto.service.generator.core.antlr.descriptor.TypeDescriptor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -27,8 +23,11 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 
-import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.exactpro.th2.proto.service.generator.core.antlr.descriptor.ServiceDescriptor;
+import com.exactpro.th2.proto.service.generator.core.antlr.descriptor.TypeDescriptor;
 
 public class ServiceClassGenerator {
 
@@ -140,11 +139,14 @@ public class ServiceClassGenerator {
 
 
         for (var method : sd.getMethods()) {
+
+            String methodName = Character.toLowerCase(method.getName().charAt(0)) + method.getName().substring(1);
+
             data.append(method.getCommentsAsJavaDoc(SERVICE_INDENT))
                     .append(LINE_SEPARATOR)
                     .append(SERVICE_INDENT)
                     .append(method.getResponseType().getNameAsParam()).append(" ")
-                    .append(method.getName()).append("(");
+                    .append(methodName).append("(");
 
             var reqTypes = method.getRequestTypes();
 
