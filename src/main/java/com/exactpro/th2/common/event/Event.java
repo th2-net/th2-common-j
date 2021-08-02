df62a476b7028d43c77bd41e3da3f77f38aa7050
Nikita Smirnov <46124551+Nikita-Smirnov-Exactpro@users.noreply.github.com>
2021-08-02 09:55:21 +0300

    Migrated the Event.disperseToBatches extension method to Event (#101) (#102)
    * Migrated the Event.disperseToBatches extension method to Event
    * Added logic to support null value in the toListBatchProto and toBatchProto methods

--- src/main/java/com/exactpro/th2/common/event/Event.java
+++ src/main/java/com/exactpro/th2/common/event/Event.java
@@ -19,19 +19,28 @@ import static com.exactpro.th2.common.event.EventUtils.createMessageBean;
 import static com.exactpro.th2.common.event.EventUtils.generateUUID;
 import static com.exactpro.th2.common.event.EventUtils.toEventID;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+import static com.google.protobuf.TextFormat.shortDebugString;
 import static java.util.Objects.requireNonNull;
+import static java.util.Objects.requireNonNullElse;
 import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.exactpro.th2.common.grpc.EventBatch;
+import com.exactpro.th2.common.grpc.EventBatchOrBuilder;
 import com.exactpro.th2.common.grpc.EventID;
 import com.exactpro.th2.common.grpc.EventStatus;
 import com.exactpro.th2.common.grpc.MessageID;
@@ -41,9 +50,11 @@ import com.google.protobuf.ByteString;
 import com.google.protobuf.Timestamp;
 
 public class Event {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Event.class);
 
     public static final String UNKNOWN_EVENT_NAME = "Unknown event name";
     public static final String UNKNOWN_EVENT_TYPE = "Unknown event type";
+    public static final EventID DEFAULT_EVENT_ID = EventID.getDefaultInstance();
 
     protected static final ThreadLocal<ObjectMapper> OBJECT_MAPPER = ThreadLocal.withInitial(() -> new ObjectMapper().setSerializationInclusion(NON_NULL));
 
@@ -64,7 +75,7 @@ public class Event {
     }
 
     protected Event(Instant startTimestamp) {
-        this(Instant.now(), null);
+        this(startTimestamp, null);
     }
 
     protected Event() {
@@ -202,7 +213,7 @@ public class Event {
      * @return current event
      */
     public Event exception(@NotNull Throwable throwable, boolean includeCauses) {
-        Throwable error = Objects.requireNonNull(throwable, "Throwable can't be null");
+        Throwable error = requireNonNull(throwable, "Throwable can't be null");
         do {
             bodyData(createMessageBean(error.toString()));
             error = error.getCause();
@@ -255,7 +266,7 @@ public class Event {
                 .setType(defaultIfBlank(type, UNKNOWN_EVENT_TYPE))
                 .setStartTimestamp(toTimestamp(startTimestamp))
                 .setEndTimestamp(toTimestamp(endTimestamp))
-                .setStatus(getAggrigatedStatus().eventStatus)
+                .setStatus(getAggregatedStatus().eventStatus)
                 .setBody(ByteString.copyFrom(buildBody()));
         if (parentID != null) {
             eventBuilder. setParentId(parentID);
@@ -264,6 +275,51 @@ public class Event {
             eventBuilder.addAttachedMessageIds(messageID);
         }
         return eventBuilder.build();
+    }
+
+    public EventBatch toBatchProto(@Nullable EventID parentID) throws JsonProcessingException {
+        List<com.exactpro.th2.common.grpc.Event> events = toListProto(parentID);
+
+        EventBatch.Builder builder = EventBatch.newBuilder()
+            .addAllEvents(events);
+
+        if (parentID != null && events.size() != 1) {
+            builder.setParentEventId(parentID);
+        }
+
+        return builder.build();
+    }
+
+    /**
+     * Converts the event with all child events to a sequence of the th2 events then organizes them into batches according to event tree structure and value of max event batch content size argent.
+     * Splitting to batch executes by principles:
+     * * Events with children are put into distinct batches because events can't be a child of an event from another batch.
+     * * Events without children are collected into batches according to the max size. For example, little child events can be put into one batch; big child events can be put into separate batches.
+     * @param maxEventBatchContentSize - the maximum size of useful content in one batch which is calculated as the sum of the size of all event bodies in the batch
+     * @param parentID - reference to parent event for the current event tree. It may be null if the current event is root.
+     */
+    public List<EventBatch> toBatchesProtoWithLimit(int maxEventBatchContentSize, @Nullable EventID parentID) throws JsonProcessingException {
+        if (maxEventBatchContentSize <= 0) {
+            throw new IllegalArgumentException("'maxEventBatchContentSize' should be greater than zero, actual: " + maxEventBatchContentSize);
+        }
+
+        List<com.exactpro.th2.common.grpc.Event> events = toListProto(parentID);
+
+        List<EventBatch> result = new ArrayList<>();
+        Map<EventID, List<com.exactpro.th2.common.grpc.Event>> eventGroups = events.stream().collect(Collectors.groupingBy(com.exactpro.th2.common.grpc.Event::getParentId));
+        batch(maxEventBatchContentSize, result, eventGroups, parentID);
+        return result;
+    }
+
+    /**
+     * Converts the event with all child events to a sequence of the th2 events then organizes them into batches according to event tree structure.
+     * Splitting to batch executes by principles:
+     * * Events with children are put into distinct batches because events can't be a child of an event from another batch.
+     * * Events without children are collected into batches.
+     * @param parentID - reference to parent event for the current event tree. It may be null if the current event is root.
+     */
+    public List<EventBatch> toListBatchProto(@Nullable EventID parentID) throws JsonProcessingException {
+        return toBatchesProtoWithLimit(Integer.MAX_VALUE, parentID);
     }
 
     public String getId() {
@@ -302,14 +358,91 @@ public class Event {
         return fieldName + " in event '" + id + "' already sed with value '" + value + '\'';
     }
 
-    @NotNull
+    /**
+     * @deprecated use {@link #getAggregatedStatus} instead
+     */
+    @Deprecated(forRemoval = true)
     protected Status getAggrigatedStatus() {
+        return getAggregatedStatus();
+    }
+
+    @NotNull
+    protected Status getAggregatedStatus() {
         if (status == Status.PASSED) {
-            return subEvents.stream().anyMatch(subEvent -> subEvent.getAggrigatedStatus() == Status.FAILED)
+            return subEvents.stream().anyMatch(subEvent -> subEvent.getAggregatedStatus() == Status.FAILED)
                     ? Status.FAILED
                     : Status.PASSED;
         }
         return Status.FAILED;
+    }
+
+    private void batch(int maxEventBatchContentSize, List<EventBatch> result, Map<EventID, List<com.exactpro.th2.common.grpc.Event>> eventGroups, @Nullable EventID eventID) throws JsonProcessingException {
+        eventID = requireNonNullElse(eventID, DEFAULT_EVENT_ID);
+        EventBatch.Builder builder = setParentId(EventBatch.newBuilder(), eventID);
+
+        List<com.exactpro.th2.common.grpc.Event> events = Objects.requireNonNull(eventGroups.get(eventID),
+                eventID == DEFAULT_EVENT_ID
+                        ? "Neither of events is root event"
+                        : "Neither of events refers to " + shortDebugString(eventID));
+
+        for (var protoEvent : events) {
+            var checkedProtoEvent = checkAndRebuild(maxEventBatchContentSize, protoEvent);
+
+            LOGGER.trace("Process {} {}", checkedProtoEvent.getName(), checkedProtoEvent.getType());
+            if(eventGroups.containsKey(checkedProtoEvent.getId())) {
+                result.add(checkAndBuild(maxEventBatchContentSize, EventBatch.newBuilder()
+                        .addEvents(checkedProtoEvent)));
+
+                batch(maxEventBatchContentSize, result, eventGroups, checkedProtoEvent.getId());
+            } else {
+                if(builder.getEventsCount() > 0
+                        && getContentSize(builder) + getContentSize(checkedProtoEvent) > maxEventBatchContentSize) {
+                    result.add(checkAndBuild(maxEventBatchContentSize, builder));
+                    builder = setParentId(EventBatch.newBuilder(), eventID);
+                }
+                builder.addEvents(checkedProtoEvent);
+            }
+        }
+
+        if(builder.getEventsCount() > 0) {
+            result.add(checkAndBuild(maxEventBatchContentSize, builder));
+        }
+    }
+
+    private EventBatch checkAndBuild(int maxEventBatchContentSize, EventBatch.Builder builder) {
+        int contentSize = getContentSize(builder);
+        if(contentSize > maxEventBatchContentSize) {
+            throw new IllegalStateException("The smallest batch size exceeds the max event batch content size, max " + maxEventBatchContentSize + ", actual " + contentSize);
+        }
+
+        return builder.build();
+    }
+
+    private com.exactpro.th2.common.grpc.Event checkAndRebuild(int maxEventBatchContentSize, com.exactpro.th2.common.grpc.Event event) throws JsonProcessingException {
+        int contentSize = getContentSize(event);
+        if (contentSize > maxEventBatchContentSize) {
+            return com.exactpro.th2.common.grpc.Event.newBuilder(event)
+                    .setStatus(EventStatus.FAILED)
+                    .setBody(ByteString.copyFrom(OBJECT_MAPPER.get().writeValueAsBytes(
+                            Collections.singletonList(createMessageBean("Event " + shortDebugString(event.getId()) + " exceeds max size, max " + maxEventBatchContentSize + ", actual " + contentSize)))))
+                    .build();
+        }
+        return event;
+    }
+
+    private static EventBatch.Builder setParentId(EventBatch.Builder builder, @NotNull EventID parentId) {
+        if (parentId != DEFAULT_EVENT_ID) {
+            builder.setParentEventId(parentId);
+        }
+        return builder;
+    }
+
+    private static int getContentSize(com.exactpro.th2.common.grpc.Event event) {
+        return event.getBody().size();
+    }
+
+    private static int getContentSize(EventBatchOrBuilder eventBatch) {
+        return eventBatch.getEventsList().stream().map(Event::getContentSize).mapToInt(Integer::intValue).sum();
     }
 
     public enum Status {
--- src/test/kotlin/com/exactpro/th2/common/event/TestEvent.kt
+++ src/test/kotlin/com/exactpro/th2/common/event/TestEvent.kt
@@ -15,33 +15,47 @@
  */
 package com.exactpro.th2.common.event
 
+import com.exactpro.th2.common.event.Event.UNKNOWN_EVENT_NAME
+import com.exactpro.th2.common.event.Event.UNKNOWN_EVENT_TYPE
+import com.exactpro.th2.common.event.EventUtils.toEventID
+import com.exactpro.th2.common.grpc.EventBatch
 import com.exactpro.th2.common.grpc.EventID
+import com.exactpro.th2.common.grpc.EventStatus.FAILED
+import com.exactpro.th2.common.grpc.EventStatus.SUCCESS
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.protobuf.ByteString
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 
+typealias ProtoEvent = com.exactpro.th2.common.grpc.Event
+
 class TestEvent {
 
-    @Test
-    fun `set parent to the toProto method`() {
-        val event = Event.start()
-        val parentEventId = EventID.newBuilder().apply {
-            id = "test"
-        }.build()
+    private val parentEventId: EventID = toEventID("parentEventId")!!
+    private val data = EventUtils.createMessageBean("0123456789".repeat(20))
+    private val dataSize = MAPPER.writeValueAsBytes(listOf(data)).size
+    private val bigData = EventUtils.createMessageBean("0123456789".repeat(30))
 
-        assertAll(
-            { assertEquals(parentEventId, event.toProto(parentEventId).parentId) },
-            { assertFalse(event.toProto(null).hasParentId()) }
-        )
+    @Test
+    fun `call the toProto method on a simple event`() {
+        Event.start().toProto(null).run {
+            checkDefaultEventFields()
+            assertFalse(hasParentId())
+        }
+
+        Event.start().toProto(parentEventId).run {
+            checkDefaultEventFields()
+            assertEquals(parentEventId, parentId)
+        }
     }
 
     @Test
     fun `set parent to the toListProto method`() {
         val event = Event.start()
-        val parentEventId = EventID.newBuilder().apply {
-            id = "test"
-        }.build()
 
         val toListProtoWithParent = event.toListProto(parentEventId)
         val toListProtoWithoutParent = event.toListProto(null)
@@ -51,5 +65,245 @@ class TestEvent {
             { assertEquals(parentEventId, toListProtoWithParent[0].parentId) },
             { assertFalse(toListProtoWithoutParent[0].hasParentId()) }
         )
+    }
+
+    @Test
+    fun `negative or zero max size`() {
+        val rootEvent = Event.start()
+        assertAll(
+            { Assertions.assertThrows(IllegalArgumentException::class.java) { rootEvent.toBatchesProtoWithLimit(-1, parentEventId) } },
+            { Assertions.assertThrows(IllegalArgumentException::class.java) { rootEvent.toBatchesProtoWithLimit(0, parentEventId) } }
+        )
+    }
+
+    @Test
+    fun `too low max size`() {
+        val rootEvent = Event.start()
+            .bodyData(data)
+
+        assertAll(
+            { Assertions.assertThrows(IllegalStateException::class.java) { rootEvent.toBatchesProtoWithLimit(1, parentEventId) } }
+        )
+    }
+
+    @Test
+    fun `every event to distinct batch`() {
+        val rootEvent = Event.start()
+            .bodyData(data).apply {
+                addSubEventWithSamePeriod()
+                    .bodyData(data)
+                    .addSubEventWithSamePeriod()
+                    .bodyData(data)
+            }
+
+        val batches = rootEvent.toBatchesProtoWithLimit(dataSize, parentEventId)
+        assertEquals(3, batches.size)
+        checkEventStatus(batches, 3, 0)
+    }
+
+    @Test
+    fun `problem events`() {
+        val rootEvent = Event.start()
+            .bodyData(data).apply {
+                addSubEventWithSamePeriod()
+                    .bodyData(data)
+                    .addSubEventWithSamePeriod()
+                    .bodyData(bigData)
+            }
+
+        val batches = rootEvent.toBatchesProtoWithLimit(dataSize, parentEventId)
+        assertEquals(3, batches.size)
+        checkEventStatus(batches, 2, 1)
+    }
+
+    @Test
+    fun `several events at the end of hierarchy`() {
+        val rootEvent = Event.start()
+            .bodyData(data).apply {
+                addSubEventWithSamePeriod()
+                    .bodyData(data)
+                addSubEventWithSamePeriod()
+                    .bodyData(bigData)
+                addSubEventWithSamePeriod()
+                    .bodyData(data)
+                addSubEventWithSamePeriod()
+                    .bodyData(data)
+            }
+
+        assertAll(
+            {
+                val batches = rootEvent.toBatchesProtoWithLimit(dataSize, parentEventId)
+                assertEquals(5, batches.size)
+                checkEventStatus(batches, 4, 1)
+            }, {
+                val batches = rootEvent.toBatchesProtoWithLimit(dataSize * 2, parentEventId)
+                assertEquals(4, batches.size)
+                checkEventStatus(batches, 5, 0)
+            }, {
+                val batches = rootEvent.toBatchesProtoWithLimit(dataSize * 3, parentEventId)
+                assertEquals(3, batches.size)
+                checkEventStatus(batches, 5, 0)
+            }
+        )
+    }
+
+    @Test
+    fun `batch structure`() {
+        val rootEvent = Event.start()
+            .bodyData(data)
+        val subEvent1 = rootEvent.addSubEventWithSamePeriod()
+            .bodyData(data)
+        val subEvent2 = rootEvent.addSubEventWithSamePeriod()
+            .bodyData(data)
+
+        val batches = rootEvent.toBatchesProtoWithLimit(1024 * 1024, parentEventId)
+        assertEquals(2, batches.size)
+        checkEventStatus(batches, 3, 0)
+
+        assertFalse(batches[0].hasParentEventId())
+        assertEquals(parentEventId, batches[0].eventsList[0].parentId)
+        assertEquals(rootEvent.id, batches[0].eventsList[0].id.id)
+
+        assertEquals(rootEvent.id, batches[1].parentEventId.id)
+        assertEquals(rootEvent.id, batches[1].eventsList[0].parentId.id)
+        assertEquals(subEvent1.id, batches[1].eventsList[0].id.id)
+        assertEquals(rootEvent.id, batches[1].eventsList[1].parentId.id)
+        assertEquals(subEvent2.id, batches[1].eventsList[1].id.id)
+    }
+
+    @Test
+    fun `event with children is after the event without children`() {
+        val rootEvent = Event.start()
+            .bodyData(data).apply {
+                addSubEventWithSamePeriod()
+                    .bodyData(data)
+                addSubEventWithSamePeriod()
+                    .bodyData(data).apply {
+                        addSubEventWithSamePeriod()
+                            .bodyData(data)
+                    }
+            }
+
+        val batches = rootEvent.toBatchesProtoWithLimit(dataSize, parentEventId)
+        assertEquals(4, batches.size)
+        checkEventStatus(batches, 4, 0)
+    }
+
+    @Test
+    fun `root event to list batch proto with size limit`() {
+        val rootName = "root"
+        val childName = "child"
+        val rootEvent = Event.start().apply {
+            name = rootName
+            bodyData(data).apply {
+                addSubEventWithSamePeriod().apply {
+                    name = childName
+                    bodyData(data)
+                }
+            }
+        }
+
+        val batches = rootEvent.toBatchesProtoWithLimit(dataSize, null)
+        assertEquals(2, batches.size)
+        checkEventStatus(batches, 2, 0)
+
+        batches[0].checkEventBatch(false, listOf(rootName))
+        batches[1].checkEventBatch(true, listOf(childName))
+    }
+
+    @Test
+    fun `root event to list batch proto without size limit`() {
+        val rootName = "root"
+        val childName = "child"
+        val rootEvent = Event.start().apply {
+            name = rootName
+            bodyData(data).apply {
+                addSubEventWithSamePeriod().apply {
+                    name = childName
+                    bodyData(data)
+                }
+            }
+        }
+
+        val batch = rootEvent.toBatchProto(null)
+        checkEventStatus(listOf(batch), 2, 0)
+        batch.checkEventBatch(false, listOf(rootName, childName))
+    }
+
+    @Test
+    fun `event with children is before the event without children`() {
+        val rootEvent = Event.start()
+            .bodyData(data).apply {
+                addSubEventWithSamePeriod()
+                    .bodyData(data).apply {
+                        addSubEventWithSamePeriod()
+                            .bodyData(data)
+                    }
+                addSubEventWithSamePeriod()
+                    .bodyData(data)
+            }
+
+        val batches = rootEvent.toBatchesProtoWithLimit(dataSize, parentEventId)
+        assertEquals(4, batches.size)
+        checkEventStatus(batches, 4, 0)
+    }
+
+    @Test
+    fun `pack event tree to single batch`() {
+        val rootEvent = Event.start()
+            .bodyData(data).apply {
+                addSubEventWithSamePeriod()
+                    .bodyData(data).apply {
+                        addSubEventWithSamePeriod()
+                            .bodyData(data)
+                    }
+                addSubEventWithSamePeriod()
+                    .bodyData(data)
+            }
+
+        val batch = rootEvent.toBatchProto(parentEventId)
+        assertEquals(parentEventId, batch.parentEventId)
+        checkEventStatus(listOf(batch), 4, 0)
+    }
+
+    @Test
+    fun `pack single event single batch`() {
+        val rootEvent = Event.start()
+
+        val batch = rootEvent.toBatchProto(parentEventId)
+        assertFalse(batch.hasParentEventId())
+        checkEventStatus(listOf(batch), 1, 0)
+    }
+
+    private fun com.exactpro.th2.common.grpc.Event.checkDefaultEventFields() {
+        assertAll(
+            { assertTrue(hasId()) },
+            { assertEquals(UNKNOWN_EVENT_NAME, name) },
+            { assertEquals(UNKNOWN_EVENT_TYPE, type) },
+            { assertTrue(hasStartTimestamp()) },
+            { assertTrue(hasEndTimestamp()) },
+            { assertEquals(SUCCESS, status) },
+            { assertEquals(ByteString.copyFrom("[]".toByteArray()), body) },
+            { assertEquals(0, attachedMessageIdsCount) }
+        )
+    }
+
+    private fun EventBatch.checkEventBatch(hasParentId: Boolean, eventNames: List<String>) {
+        assertEquals(hasParentId, hasParentEventId())
+        assertEquals(eventNames.size, eventsCount)
+        assertEquals(eventNames, eventsList.map(ProtoEvent::getName).toList())
+    }
+
+    private fun checkEventStatus(batches: List<EventBatch>, successNumber: Int, filedNumber: Int) {
+        val events = batches.flatMap(EventBatch::getEventsList)
+        assertAll(
+            { assertEquals(filedNumber + successNumber, events.size, "number") },
+            { assertEquals(filedNumber, events.filter { it.status == FAILED }.size, "success") },
+            { assertEquals(successNumber, events.filter { it.status == SUCCESS }.size, "failed") }
+        )
+    }
+
+    companion object {
+        private val MAPPER = ObjectMapper()
     }
 }
