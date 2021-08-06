389cff5308d59f4075beccea5ead242d214b82fe
ivan.druzhinin <ivan.druzhinin@exactprosystems.com>
2021-08-06 15:55:56 +0300

    Add missing copyright

--- src/main/kotlin/com/exactpro/th2/common/schema/configuration/Secrets.kt
+++ src/main/kotlin/com/exactpro/th2/common/schema/configuration/Secrets.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021-2021 Exactpro (Exactpro Systems Limited)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.exactpro.th2.common.schema.configuration
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter
@@ -5,7 +20,7 @@ import com.fasterxml.jackson.annotation.JsonAnySetter
 
 class Secrets {
     private val secrets = mutableMapOf<String, String>()
-    @JsonAnyGetter get() = field
+        @JsonAnyGetter get() = field
 
     operator fun get(name: String): String? = secrets[name]
     @JsonAnySetter private fun set(name: String, value: String) = secrets.set(name, value)
