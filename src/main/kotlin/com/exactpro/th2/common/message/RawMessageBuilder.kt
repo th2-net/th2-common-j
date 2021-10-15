dc557a15b316ebfa6ca51072c58b07ac53e5dce3
andrey.drobynin <andrey.drobynin@exactprosystems.com>
2021-10-15 21:28:24 +0400

    corrected javadoc

--- src/main/kotlin/com/exactpro/th2/common/message/RawMessageBuilder.kt
+++ src/main/kotlin/com/exactpro/th2/common/message/RawMessageBuilder.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.exactpro.th2.common.message
 
 import com.exactpro.th2.common.grpc.EventID
