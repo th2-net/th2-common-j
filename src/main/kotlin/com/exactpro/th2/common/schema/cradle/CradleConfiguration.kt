cb084216422e3627c8777c69650839403fb8eb76
Andrey Drobynin <89458084+andreydrobynin@users.noreply.github.com>
2021-11-22 22:33:02 +0400

    [th2-2689] adaptation to book/page cradleapi (#145)

--- src/main/kotlin/com/exactpro/th2/common/schema/cradle/CradleConfiguration.kt
+++ src/main/kotlin/com/exactpro/th2/common/schema/cradle/CradleConfiguration.kt
@@ -15,6 +15,7 @@
 
 package com.exactpro.th2.common.schema.cradle
 
+import com.exactpro.cradle.CradleStorage
 import com.exactpro.cradle.cassandra.CassandraStorageSettings
 import com.exactpro.th2.common.schema.configuration.Configuration
 import com.fasterxml.jackson.annotation.JsonProperty
@@ -27,12 +28,11 @@ data class CradleConfiguration(
     var port: Int,
     var username: String?,
     var password: String?,
-    var cradleInstanceName: String?,
     var timeout: Long,
     var pageSize: Int,
-    var cradleMaxEventBatchSize: Long,
-    var cradleMaxMessageBatchSize: Long
-)  : Configuration() {
+    var cradleMaxEventBatchSize: Int,
+    var cradleMaxMessageBatchSize: Int
+) : Configuration() {
     constructor(
         cradleConfidentialConfiguration: CradleConfidentialConfiguration,
         cradleNonConfidentialConfiguration: CradleNonConfidentialConfiguration
@@ -43,7 +43,6 @@ data class CradleConfiguration(
         cradleConfidentialConfiguration.port,
         cradleConfidentialConfiguration.username,
         cradleConfidentialConfiguration.password,
-        cradleConfidentialConfiguration.cradleInstanceName,
         cradleNonConfidentialConfiguration.timeout,
         cradleNonConfidentialConfiguration.pageSize,
         cradleNonConfidentialConfiguration.cradleMaxEventBatchSize,
@@ -57,13 +56,12 @@ data class CradleConfidentialConfiguration(
     @JsonProperty(required = true) var keyspace: String,
     var port: Int = 0,
     var username: String? = null,
-    var password: String? = null,
-    var cradleInstanceName: String? = null
+    var password: String? = null
 ) : Configuration()
 
 data class CradleNonConfidentialConfiguration(
     var timeout: Long = CassandraStorageSettings.DEFAULT_TIMEOUT,
     var pageSize: Int = 5000,
-    var cradleMaxEventBatchSize: Long = CassandraStorageSettings.DEFAULT_MAX_EVENT_BATCH_SIZE,
-    var cradleMaxMessageBatchSize: Long = CassandraStorageSettings.DEFAULT_MAX_MESSAGE_BATCH_SIZE
+    var cradleMaxEventBatchSize: Int = CradleStorage.DEFAULT_MAX_TEST_EVENT_BATCH_SIZE,
+    var cradleMaxMessageBatchSize: Int = CradleStorage.DEFAULT_MAX_MESSAGE_BATCH_SIZE
 ) : Configuration()
