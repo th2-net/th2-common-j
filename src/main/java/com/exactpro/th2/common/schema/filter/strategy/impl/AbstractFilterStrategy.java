b3a2a182680656ac13a1acddc035ef8c93ac6d2b
Rekish <fed_rekish@hotmail.com>
2022-05-13 16:37:27 +0400

    [TH2-3673] Minor optimization for verifying against multiple filters

--- src/main/java/com/exactpro/th2/common/schema/filter/strategy/impl/AbstractFilterStrategy.java
+++ src/main/java/com/exactpro/th2/common/schema/filter/strategy/impl/AbstractFilterStrategy.java
@@ -21,6 +21,7 @@ import com.exactpro.th2.common.schema.message.configuration.RouterFilter;
 import com.google.protobuf.Message;
 import org.apache.commons.collections4.MultiMapUtils;
 import org.apache.commons.collections4.MultiValuedMap;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.List;
@@ -30,23 +31,33 @@ public abstract class AbstractFilterStrategy<T extends Message> implements Filte
 
     @Override
     public boolean verify(T message, RouterFilter routerFilter) {
-
-        MultiValuedMap<String, FieldFilterConfiguration> msgFieldFilters = MultiMapUtils.newListValuedHashMap();
-        msgFieldFilters.putAll(routerFilter.getMessage());
-        msgFieldFilters.putAll(routerFilter.getMetadata());
-
+        MultiValuedMap<String, FieldFilterConfiguration> msgFieldFilters = extractConfigMapFromRouterFilter(routerFilter);
         return checkValues(getFields(message), msgFieldFilters);
     }
 
     @Override
     public boolean verify(T message, List<? extends RouterFilter> routerFilters) {
+        Map<String, String> fields = getFields(message);
         for (var fieldsFilter : routerFilters) {
-            if (verify(message, fieldsFilter)) {
+            if (verify(fields, fieldsFilter)) {
                 return true;
             }
         }
 
         return false;
+    }
+
+    private boolean verify(Map<String, String> fields, RouterFilter routerFilter) {
+        MultiValuedMap<String, FieldFilterConfiguration> msgFieldFilters = extractConfigMapFromRouterFilter(routerFilter);
+        return checkValues(fields, msgFieldFilters);
+    }
+
+    @NotNull
+    private MultiValuedMap<String, FieldFilterConfiguration> extractConfigMapFromRouterFilter(RouterFilter routerFilter) {
+        MultiValuedMap<String, FieldFilterConfiguration> msgFieldFilters = MultiMapUtils.newListValuedHashMap();
+        msgFieldFilters.putAll(routerFilter.getMessage());
+        msgFieldFilters.putAll(routerFilter.getMetadata());
+        return msgFieldFilters;
     }
 
     protected abstract Map<String, String> getFields(T message);
