d68bcefde94e2c5cdb4d2018ae78804eb2c24f6f
Rekish <fed_rekish@hotmail.com>
2022-05-30 12:49:46 +0400

    [TH2-2908] removed debug commit

--- src/main/java/com/exactpro/th2/common/schema/util/Log4jConfigUtils.kt
+++ src/main/java/com/exactpro/th2/common/schema/util/Log4jConfigUtils.kt
@@ -26,8 +26,7 @@ class Log4jConfigUtils {
                 )
             }
             .filter { Files.exists(it.first) }
-//            .run { if (secondVersionPriority) sortedByDescending { it.second } else this }
-            .run { if (secondVersionPriority) sortedBy { it.second } else this }
+            .run { if (secondVersionPriority) sortedByDescending { it.second } else this }
             .getOrNull(0)
 
         when (configPathList?.second) {
