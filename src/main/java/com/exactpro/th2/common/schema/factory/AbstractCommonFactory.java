893d2a5329d5cba0db4f2c1e72c01ae62f5a5b1f
Luka Tchumburidze <lukatchumburidze@gmail.com>
2022-12-26 05:58:02 +0400

    [TH2-4597] fix

--- src/main/java/com/exactpro/th2/common/schema/factory/AbstractCommonFactory.java
+++ src/main/java/com/exactpro/th2/common/schema/factory/AbstractCommonFactory.java
@@ -533,7 +533,7 @@ public abstract class AbstractCommonFactory implements AutoCloseable {
                     if (nonConfidentialConfiguration.getCradleMaxEventBatchSize() > 0) {
                         cassandraStorageSettings.setMaxTestEventBatchSize((int) nonConfidentialConfiguration.getCradleMaxEventBatchSize());
                     }
-                    cassandraStorageSettings.setBookRefreshIntervalMillis(nonConfidentialConfiguration.getStatisticsPersistenceIntervalMillis());
+                    cassandraStorageSettings.setCounterPersistenceInterval((int) nonConfidentialConfiguration.getStatisticsPersistenceIntervalMillis());
 
                     manager = new CassandraCradleManager(
                             cassandraConnectionSettings,
