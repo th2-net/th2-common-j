37fac82f27741d852ee7ab5d3887bdef291fd7db
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2023-03-13 10:13:53 +0400

    Added FIXMEs afret profiling

--- src/main/kotlin/com/exactpro/th2/common/metrics/MetricsUtils.kt
+++ src/main/kotlin/com/exactpro/th2/common/metrics/MetricsUtils.kt
@@ -72,6 +72,7 @@ data class SessionStats(
     var rawMessages: Int = 0,
 )
 
+//FIXME: com.exactpro.th2.common.metrics.MetricsUtilsKt.incrementTotalMetrics() 30,374 ms (12.5%)
 fun incrementTotalMetrics(
     batch: MessageGroupBatch,
     th2Pin: String,
@@ -82,6 +83,7 @@ fun incrementTotalMetrics(
     val incomingStatsBySession = mutableMapOf<String, SessionStats>()
     val outgoingStatsBySession = mutableMapOf<String, SessionStats>()
 
+    //FIXME: java.util.Collections$UnmodifiableCollection.iterator() 14,357 ms (5.9%)
     for (group in batch.groupsList) {
         val messages = group.messagesList
 
