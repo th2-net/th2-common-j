424e216d3078786439c775c29b170d8390da0278
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2024-04-12 15:31:11 +0400

    Use box name from `box.json` config as RabbitMQ connection name

--- src/main/java/com/exactpro/th2/common/schema/factory/AbstractCommonFactory.java
+++ src/main/java/com/exactpro/th2/common/schema/factory/AbstractCommonFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2023 Exactpro (Exactpro Systems Limited)
+ * Copyright 2020-2024 Exactpro (Exactpro Systems Limited)
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -658,7 +658,7 @@ public abstract class AbstractCommonFactory implements AutoCloseable {
     }
 
     protected ConnectionManager createRabbitMQConnectionManager() {
-        return new ConnectionManager(getRabbitMqConfiguration(), getConnectionManagerConfiguration());
+        return new ConnectionManager(getBoxConfiguration().getBoxName(), getRabbitMqConfiguration(), getConnectionManagerConfiguration());
     }
 
     protected ConnectionManager getRabbitMqConnectionManager() {
--- src/test/kotlin/com/exactpro/th2/common/schema/message/impl/rabbitmq/transport/TransportGroupBatchRouterIntegrationTest.kt
+++ src/test/kotlin/com/exactpro/th2/common/schema/message/impl/rabbitmq/transport/TransportGroupBatchRouterIntegrationTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Exactpro (Exactpro Systems Limited)
+ * Copyright 2023-2024 Exactpro (Exactpro Systems Limited)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -123,6 +123,7 @@ class TransportGroupBatchRouterIntegrationTest {
         prefetchCount: Int = DEFAULT_PREFETCH_COUNT,
         confirmationTimeout: Duration = DEFAULT_CONFIRMATION_TIMEOUT
     ) = ConnectionManager(
+        "test-connection",
         RabbitMQConfiguration(
             host = rabbitMQContainer.host,
             vHost = "",
