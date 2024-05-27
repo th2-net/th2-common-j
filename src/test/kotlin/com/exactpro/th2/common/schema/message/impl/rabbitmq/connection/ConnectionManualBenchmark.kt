09f49bdd5b25386cf5c023100c7e20be3d543738
Oleg <oleg.smirnov@exactprosystems.com>
2024-05-27 15:27:25 +0400

    Manual publication test

---
+++ src/test/kotlin/com/exactpro/th2/common/schema/message/impl/rabbitmq/connection/ConnectionManualBenchmark.kt
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2024 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.exactpro.th2.common.schema.message.impl.rabbitmq.connection
+
+import com.exactpro.th2.common.schema.message.ContainerConstants.RABBITMQ_IMAGE_NAME
+import com.exactpro.th2.common.schema.message.impl.rabbitmq.configuration.ConnectionManagerConfiguration
+import com.exactpro.th2.common.schema.message.impl.rabbitmq.configuration.RabbitMQConfiguration
+import mu.KotlinLogging
+import org.testcontainers.containers.RabbitMQContainer
+import java.time.Duration
+import java.util.concurrent.ThreadLocalRandom
+
+object ConnectionManualBenchmark {
+    private val LOGGER = KotlinLogging.logger {}
+    @JvmStatic
+    fun main(args: Array<String>) {
+        RabbitMQContainer(RABBITMQ_IMAGE_NAME).use { container ->
+            val payload: ByteArray = ByteArray(256 * 1024).also {
+                ThreadLocalRandom.current().nextBytes(it)
+            }
+            container.start()
+            with(container) {
+                val queueName = "test-queue"
+                val exchangeName = "test-exchange"
+                execInContainer("rabbitmqadmin", "declare", "queue", "name=$queueName", "durable=false")
+                execInContainer("rabbitmqadmin", "declare", "exchange", "name=$exchangeName", "type=fanout")
+                execInContainer("rabbitmqadmin", "declare", "binding", "source=$exchangeName", "destination_type=queue", "destination=$queueName")
+
+                val consumer = createConnectionManager(
+                    container,
+                    ConnectionManagerConfiguration(
+                        prefetchCount = 1000,
+                        confirmationTimeout = Duration.ofSeconds(5),
+                        enablePublisherConfirmation = true,
+                        maxInflightPublications = 100,
+                    )
+                )
+
+                val connectionManagerWithConfirmation = {
+                    createConnectionManager(
+                        container,
+                        ConnectionManagerConfiguration(
+                            prefetchCount = 100,
+                            confirmationTimeout = Duration.ofSeconds(5),
+                            enablePublisherConfirmation = true,
+                            maxInflightPublications = 200,
+                        )
+                    )
+                }
+
+                val connectionManagerWithoutConfirmation = {
+                    createConnectionManager(
+                        container,
+                        ConnectionManagerConfiguration(
+                            prefetchCount = 100,
+                            confirmationTimeout = Duration.ofSeconds(5),
+                            enablePublisherConfirmation = false,
+                            maxInflightPublications = -1,
+                        )
+                    )
+                }
+
+                consumer.use {
+                    consumer.basicConsume(
+                        queueName,
+                        { _, _, ack -> ack.confirm()  },
+                        { LOGGER.warn { "Canceled" } },
+                    )
+                    val times = 5
+                    val withConf = ArrayList<Duration>(times)
+                    val withoutConf = ArrayList<Duration>(times)
+                    repeat(times) {
+                    // Avg for no confirmation: PT19.468S
+                        withoutConf += measure("not confirmation", connectionManagerWithoutConfirmation, payload)
+
+                    // Avg for confirmation: PT20.758S
+                        withConf += measure("confirmation", connectionManagerWithConfirmation, payload)
+                    }
+
+                    fun List<Duration>.avg(): Duration {
+                        return map { it.toMillis() }.average().let { Duration.ofMillis(it.toLong()) }
+                    }
+
+                    LOGGER.info { "Avg for confirmation: ${withConf.avg()}" }
+                    LOGGER.info { "Avg for no confirmation: ${withoutConf.avg()}" }
+                }
+            }
+        }
+    }
+
+    private fun measure(name: String, manager: () -> ConnectionManager, payload: ByteArray): Duration {
+        LOGGER.info("Measuring $name")
+        val start: Long
+        val sent: Long
+        manager().use { mgr ->
+            repeat(100) {
+                mgr.basicPublish(
+                    "test-exchange",
+                    "routing",
+                    null,
+                    payload,
+                )
+            }
+            LOGGER.info("Wait after warmup for $name")
+            Thread.sleep(1000)
+            LOGGER.info("Start measuring for $name")
+            start = System.currentTimeMillis()
+            repeat(100_000) {
+                mgr.basicPublish(
+                    "test-exchange",
+                    "routing",
+                    null,
+                    payload,
+                )
+            }
+            sent = System.currentTimeMillis()
+        }
+        val end = System.currentTimeMillis()
+        LOGGER.info("Sent $name in ${Duration.ofMillis(sent - start)}")
+        val duration = Duration.ofMillis(end - start)
+        LOGGER.info("Executed $name in $duration")
+        return duration
+    }
+
+    private fun createConnectionManager(container: RabbitMQContainer, configuration: ConnectionManagerConfiguration) =
+        ConnectionManager(
+            "test-connection",
+            RabbitMQConfiguration(
+                host = container.host,
+                vHost = "",
+                port = container.amqpPort,
+                username = container.adminUsername,
+                password = container.adminPassword,
+            ),
+            configuration
+        )
+}
