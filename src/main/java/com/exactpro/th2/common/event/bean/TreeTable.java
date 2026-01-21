57e38f2c31b62a24713eb1a6b08356aa565a4e27
Nikita Smirnov <46124551+Nikita-Smirnov-Exactpro@users.noreply.github.com>
2026-01-21 12:28:25 +0400

    Updated copyright (#420)

--- src/main/java/com/exactpro/th2/common/event/bean/TreeTable.java
+++ src/main/java/com/exactpro/th2/common/event/bean/TreeTable.java
@@ -1,5 +1,5 @@
-/******************************************************************************
- * Copyright 2020-2021 Exactpro (Exactpro Systems Limited)
+/*
+ * Copyright 2020-2026 Exactpro (Exactpro Systems Limited)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ */
 package com.exactpro.th2.common.event.bean;
 
 import com.exactpro.th2.common.event.IBodyData;
